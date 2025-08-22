b29aa2c0d9c9b5893add71bf8cda323ed36b07b5
rezaulislam <mrezaulislam.t@gmail.com>
2025-08-22 08:29:26 +0600

    Initial commit

---
+++ My Friends/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  My Friends
+//
+//  Created by Rezaul Islam on 22/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ My Friends/My_FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_FriendsApp.swift
+//  My Friends
+//
+//  Created by Rezaul Islam on 22/8/25.
+//
+
+import SwiftUI
+
+@main
+struct My_FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
