e936ee4b680850e37ebc46f3c77078ae133a75e8
rezaulislam <mrezaulislam.t@gmail.com>
2025-08-22 08:38:43 +0600

    friends name data added

---
+++ My Friends/DB.swift
@@ -0,0 +1,12 @@
+//
+//  DB.swift
+//  My Friends
+//
+//  Created by Rezaul Islam on 22/8/25.
+//
+
+import Foundation
+
+final class DB {
+    static let friends : [ String] = ["Rezaul", "Rakib", "Shahriar", "Shahid", "Kamal", "Jamal", "Md. Jahangir", "Md. Jahid", "Fuad", "Md. Jahid Mia" ]
+}
